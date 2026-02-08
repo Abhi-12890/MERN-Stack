dce193747d644f180749df236976e0a57c3cf573
Abhishek Kumar <abhisinghshakya1289@gmail.com>
2026-02-08 14:13:40 +0530

    Initialize Express server with welcome route
    Set up a basic Express server with a welcome message.

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to my website.And Enjoy my Website1.");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running at Port: ${PORT}`);
+  console.log("Hello my name is Abhishek kumar.")
+
+})
