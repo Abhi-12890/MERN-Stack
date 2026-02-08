8be1140a728e740e8bbf42a55cabc88b311b3d59
Abhi-12890 <abhisinghshakya1289@gmail.com>
2026-02-08 17:16:12 +0530

    Add some Router

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const Router = require("./router/auth-router");
 
-app.get("/", (req, res) => {
-  res.status(200).send("Welcome to my website.And Enjoy my Website1232323hello.");
-});
+app.use("/api/auth", Router);
+
+// app.get("/", (req, res) => {
+//   res.status(200).send("Welcome to my website.And Enjoy my Website1232323hello.");
+// });
 
 const PORT = 5000;
 app.listen(PORT, () => {
